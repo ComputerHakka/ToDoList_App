49a2e77fa572cee8cbdd067ad4762c2948e67638
Вячеслав Решетняк <r_slavik2015@inbox.ru>
2024-03-16 15:28:03 +0300

    create app

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/app/my_app.dart';
+
+void main() {
+  const app = MyApp();
+  runApp(app);
+}
+
---
+++ lib/widgets/app/my_app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test_application/widgets/groups/groups_widget.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      routes: {
+        '/groups': (context) => const GroupsWidget(),
+      },
+      initialRoute: '/groups',
+      theme: ThemeData(      
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
+
